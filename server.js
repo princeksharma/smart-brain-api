fbda7f400c602ef7a3facf80983f42ac310838c3
Prince Sharma <sharmaprince2271997@gmail.com>
2019-06-14 01:13:53 +0530

    ..

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=> console.log(`Server running on port ${port}`));
