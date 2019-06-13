6d48139d4a4d43b3d021d51468e4afc923197d14
Prince Sharma <sharmaprince2271997@gmail.com>
2019-06-14 00:34:58 +0530

    Few Updates

--- server.js
+++ server.js
@@ -31,14 +31,4 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-const port = process.env.PORT || 5000;
-
-app.listen(port,()=> console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || 3000 ,()=> console.log(`Server running on port ${process.env.PORT}`));
