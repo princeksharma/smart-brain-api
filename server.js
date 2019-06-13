219bdc09e809891e75d4a2defea876dc7e20aff3
Prince Sharma <sharmaprince2271997@gmail.com>
2019-06-14 00:52:07 +0530

    Updated packages

--- server.js
+++ server.js
@@ -31,4 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000 ,()=> console.log(`Server running on port ${process.env.PORT}`));
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=> console.log(`Server running on port ${port}`));
